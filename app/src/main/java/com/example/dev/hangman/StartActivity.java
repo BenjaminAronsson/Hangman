46407ef6a07f73bd5381d4f3fcbc18c03ffa7405
dev <bene.a99@gmail.com>
2018-10-17 08:16:21 +0200

    added layout to startActivity

---
+++ app/src/main/java/com/example/dev/hangman/StartActivity.java
@@ -0,0 +1,29 @@
+package com.example.dev.hangman;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    public void startButtonClicked(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+
+        // Get the text view
+        //noteView  = findViewById(R.id.editText);
+        //String note = noteView.getText().toString();
+
+        //intent.putExtra("Message", note);
+        startActivity(intent);
+    }
+
+    public void InfoButtonClicked(View view) {
+    }
+}
