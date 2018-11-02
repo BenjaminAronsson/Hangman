bd65d6f15112cd636589a4cb66110f7f13021ac7
dev <bene.a99@gmail.com>
2018-11-02 10:10:37 +0100

    test

---
+++ app/src/main/java/com/example/dev/hangman/toolbar.java
@@ -0,0 +1,18 @@
+package com.example.dev.hangman;
+
+abstract class toolbar {
+
+//Visibility
+
+    protected  boolean isBackButton() {
+        return false;
+    }
+
+    protected  boolean isAboutButton() {
+        return false;
+    }
+
+    protected  boolean isPlayButton() {
+        return false;
+    }
+}
